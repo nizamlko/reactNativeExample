c21f3aab39e794e0a9d61ccaeaa85bc1079ab68e
Nizam <nizam.lko@gmail.com>
2016-06-15 02:14:16 +0530

    PropertyFinder add the search api

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -7,7 +7,7 @@ import {
   TextInput,
   View,
   TouchableHighlight,
-  ActivityIndicatorIOS,
+  ProgressBarAndroid,
   Image,
 } from 'react-native';
 
@@ -16,17 +16,75 @@ class SearchPage extends Component {
 		super(props);
 		this.state = {
 			searchString: 'london2',
+			isLoading: false,
+			message: '',
 			count:1
 		};
 	}
+	
 	onSearchTextChanged(event) {
 		console.log('onSearchTextChanged');
 		this.setState({ searchString: event.nativeEvent.text });
 		console.log(this.state.searchString);
 	}
+	
+	urlForQueryAndPage(key, value, pageNumber) {
+	  var data = {
+		  country: 'uk',
+		  pretty: '1',
+		  encoding: 'json',
+		  listing_type: 'buy',
+		  action: 'search_listings',
+		  page: pageNumber
+	  };
+	  data[key] = value;
+	 
+	  var querystring = Object.keys(data)
+		.map(key => key + '=' + encodeURIComponent(data[key]))
+		.join('&');
+	 
+	  return 'http://api.nestoria.co.uk/api?' + querystring;
+	}
+	
+	_handleResponse(response) {
+	 console.log("_handleResponse :"+ response);
+	  this.setState({ isLoading: false , message: '' });
+	  if (response.application_response_code.substr(0, 1) === '1') {
+		console.log('Properties found: ' + response.listings.length);
+		this.setState({ message: 'Properties found: ' + response.listings.length});
+	  } else {
+		this.setState({ message: 'Location not recognized; please try again.'});
+	  }
+	}
+	
+	_executeQuery(query) {
+	  console.log("query :"+ query);
+	  this.setState({ isLoading: true });
+	  fetch(query)
+	  .then(response => response.json())
+	  .then(json => this._handleResponse(json.response))
+	  .catch(error =>
+		 this.setState({
+		  isLoading: false,
+		  message: 'Something bad happened ' + error
+	   }));
+	}
+	 
+	onSearchPressed() {
+	  var query = this.urlForQueryAndPage('place_name', this.state.searchString, 1);
+	  this._executeQuery(query);
+	}
+	
+	
 	render() {
 	console.log('SearchPage.render');
 	this.state.count++;
+	var spinner = this.state.isLoading ?
+      ( <ProgressBarAndroid
+          hidden='true'
+          size='large'/> ) :
+      ( <View/>);
+
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -42,7 +100,7 @@ class SearchPage extends Component {
 			onChange={this.onSearchTextChanged.bind(this)}			
 			placeholder='Search via name or postcode'
 			/>
-		  <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+		  <TouchableHighlight style={styles.button} underlayColor='#99d9f4' onPress={this.onSearchPressed.bind(this)}>
 			<Text style={styles.buttonText}>Go</Text>
 		  </TouchableHighlight>
 		</View>
@@ -50,6 +108,8 @@ class SearchPage extends Component {
 		  <Text style={styles.buttonText}>Location</Text>
 		</TouchableHighlight>
 		<Image source={require('./Resources/house.png')} style={styles.image}/>
+		{spinner}
+		<Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
